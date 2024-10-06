a02b3b4f9753266967c1002b1f416f7c26c4e4d7
Konstantin Yeftifeyev <zoxo@outlook.com>
2024-10-06 23:45:49 +0300

    feat: Add add_user_to_project

--- core/core_service/api/src/services/project_service.rs
+++ core/core_service/api/src/services/project_service.rs
@@ -1,11 +1,13 @@
 use core_database::queries::projects_query::ProjectQuery;
 use core_debugger::tracing::{event, Level};
+use sea_orm::DbConn;
 use tonic::{Request, Response, Status};
 
 use crate::{
     helai_api_core_service::{
         projects_service_server::ProjectsService, CreateProjectRequest, CreateProjectResponse,
-        DeleteProjectRequest, ProjectUsersResponse, StatusResponse, UserProjectModificationRequest,
+        DeleteProjectRequest, ProjectUserInfoResponse, StatusResponse,
+        UserProjectModificationRequest,
     },
     middleware::{
         interceptors,
@@ -57,16 +59,6 @@ impl ProjectsService for MyServer {
         Ok(response)
     }
 
-    async fn add_user_to_project(
-        &self,
-        request: Request<UserProjectModificationRequest>,
-    ) -> Result<Response<ProjectUsersResponse>, Status> {
-        event!(target: "hellai_app_core_events", Level::DEBUG, "{:?}", request);
-        let user_id_from_token = interceptors::check_auth_token(request.metadata())?;
-        println!("user_id_from_token: {}", user_id_from_token);
-        todo!()
-    }
-
     async fn delete_project(
         &self,
         request: Request<DeleteProjectRequest>,
@@ -79,28 +71,41 @@ impl ProjectsService for MyServer {
 
         let request = request.into_inner();
 
-        // Check that user have permision Owner lvl
-        let user_role_in_project = ProjectQuery::get_user_role_in_project(
-            conn,
-            request.project_id,
-            user_id_from_token as i32,
-        )
-        .await?;
-
-        match user_role_in_project {
-            Some(user_role_in_project) => {
-                if user_role_in_project.project_role_id != 1 {
-                    return Err(Status::permission_denied("insufficient_rights"));
-                }
-            }
-            None => return Err(Status::permission_denied("insufficient_rights")),
-        }
+        check_project_permision(conn, user_id_from_token as i32, request.project_id).await?;
 
         ProjectQuery::delete_project(conn, request.project_id).await?;
 
-        println!("user_id_from_token: {}", user_id_from_token);
-
         let response = Response::new(StatusResponse { success: true });
+
+        event!(target: "hellai_app_core_events", Level::DEBUG, "{:?}", response);
+
+        Ok(response)
+    }
+
+    async fn add_user_to_project(
+        &self,
+        request: Request<UserProjectModificationRequest>,
+    ) -> Result<Response<ProjectUserInfoResponse>, Status> {
+        event!(target: "hellai_app_core_events", Level::DEBUG, "{:?}", request);
+        // Check if user auth and get his id
+        let user_id_from_token = interceptors::check_auth_token(request.metadata())?;
+
+        let conn = &self.connection;
+
+        let request = request.into_inner();
+
+        let admin_id = user_id_from_token as i32;
+        let project_id = request.project_id;
+        let user_id = request.user_id;
+
+        check_project_permision(conn, admin_id, project_id).await?;
+
+        let user_role = ProjectQuery::set_user_project_role(conn, user_id, project_id, 3).await?;
+
+        let response = Response::new(ProjectUserInfoResponse {
+            user_id: user_id,
+            user_role: user_role.project_role_id - 1,
+        });
 
         event!(target: "hellai_app_core_events", Level::DEBUG, "{:?}", response);
 
@@ -110,10 +115,32 @@ impl ProjectsService for MyServer {
     async fn remove_user_from_project(
         &self,
         request: Request<UserProjectModificationRequest>,
-    ) -> Result<Response<ProjectUsersResponse>, Status> {
+    ) -> Result<Response<ProjectUserInfoResponse>, Status> {
         event!(target: "hellai_app_core_events", Level::DEBUG, "{:?}", request);
         let user_id_from_token = interceptors::check_auth_token(request.metadata())?;
         println!("user_id_from_token: {}", user_id_from_token);
         todo!()
     }
+}
+
+// Check that user have permision to project
+async fn check_project_permision(
+    conn: &DbConn,
+    user_id: i32,
+    project_id: i32,
+) -> Result<(), Status> {
+    // Check that user have permision Owner lvl
+    let user_role_in_project =
+        ProjectQuery::get_user_role_in_project(conn, project_id, user_id).await?;
+
+    match user_role_in_project {
+        Some(user_role_in_project) => {
+            if user_role_in_project.project_role_id != 1 {
+                return Err(Status::permission_denied("insufficient_rights"));
+            }
+        }
+        None => return Err(Status::permission_denied("insufficient_rights")),
+    }
+
+    Ok(())
 }
