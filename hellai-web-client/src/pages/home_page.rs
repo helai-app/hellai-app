72951292793d444aa797dd5f4adc59997c434e1c
Konstantin Yeftifeyev <zoxo@outlook.com>
2024-10-21 20:24:55 +0300

    feat: Add redirect

--- hellai-web-client/src/pages/home_page.rs
+++ hellai-web-client/src/pages/home_page.rs
@@ -2,7 +2,14 @@
 
 use dioxus::prelude::*;
 
+use crate::utilities::cookie_manager::WebClientCookieManager;
+
 #[component]
 pub fn HomeElement() -> Element {
+    if WebClientCookieManager::get_cookie("my_key").is_none() {
+        let navigator = use_navigator();
+        navigator.replace("/login");
+    }
+
     rsx! {"Home Page"}
 }
--- hellai-web-client/src/routing.rs
+++ hellai-web-client/src/routing.rs
@@ -1,12 +1,11 @@
-// use crate::pages::home_page::*;
+use crate::pages::home_page::*;
 use crate::pages::log_in_page::*;
 use dioxus::prelude::*;
 
 #[derive(Clone, Routable, Debug, PartialEq)]
 pub enum SystemRoute {
-    // #[route("/")]
-    // HomeElement {},
-    // #[route("/auth/login")]
     #[route("/")]
+    HomeElement {},
+    #[route("/login")]
     LogInElement {},
 }
